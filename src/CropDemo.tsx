c10de640bbdde9e43f9796434b546fe6e60e1610
xiezhentian <xiezhentian@xiaomi.com>
2020-07-22 15:23:52 +0800

    test sandbox

--- src/CropDemo.tsx
+++ src/CropDemo.tsx
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 import ReactCrop from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
 export default function CropDemo(props: { src: string }) {
-  const [crop, setCrop] = useState({
-    width: 200,
-    height: 200,
-    disabled: true,
-    locked: true
+  const [crop, setCrop] = useState<ReactCrop.Crop>({
+    width: 120,
+    height: 120,
   });
   const { src } = props;
 
@@ -14,5 +12,5 @@ export default function CropDemo(props: { src: string }) {
     console.log(newCrop);
     setCrop(newCrop);
   };
-  return <ReactCrop src={src} crop={crop} onChange={handleCropChange} />;
+  return <ReactCrop src={src} crop={crop} onChange={handleCropChange} locked/>;
 }
